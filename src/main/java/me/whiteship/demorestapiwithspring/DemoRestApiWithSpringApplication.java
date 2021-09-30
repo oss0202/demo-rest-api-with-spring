5e8079f01d8360116836e23efbb6a3f9d4777037
오성식 <oss0202@etoos.com>
2021-09-30 10:11:44 +0900

    Initial commit

---
+++ src/main/java/me/whiteship/demorestapiwithspring/DemoRestApiWithSpringApplication.java
@@ -0,0 +1,13 @@
+package me.whiteship.demorestapiwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestApiWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoRestApiWithSpringApplication.class, args);
+    }
+
+}
